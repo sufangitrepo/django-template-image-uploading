a81578be86423b65b7f79fd9336f76d1666d5b7a
M Sufyan <97937721+sufangitrepo@users.noreply.github.com>
2023-05-10 13:40:24 +0500

    Update settings.py

--- image_store/settings.py
+++ image_store/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0gs^3uq0@tm#qi8p*7q61$e7ztv2m5^eojw97i-im51i!sk96!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
